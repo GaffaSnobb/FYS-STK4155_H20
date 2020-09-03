c84106e7455ed63fddcd1a73a8dc3bbdd7ff1864
jonkdahl <jon@dira.no>
2020-09-03 16:52:15 +0200

    Fix an error in R^2 and MSE. Implement the design matrix correctly.

--- projects/project1/project1.py
+++ projects/project1/project1.py
@@ -29,15 +29,12 @@ def mean_squared_error(y_observed, y_predicted):
     : numpy.ndarray
         The mean squared error.
     """
-    return np.sum(y_observed - y_predicted)**2/len(y_observed)
+    return np.sum((y_observed - y_predicted)**2)/len(y_observed)
 
 
 def r_squared(y_observed, y_predicted):
     """
     Calculate the score R**2.
-
-    Consider adding the length n as an argument if this function is
-    called many times.
 
     Parameters
     ----------
@@ -52,13 +49,15 @@ def r_squared(y_observed, y_predicted):
     : numpy.ndarray
         The R**2 score.
     """
-    return 1 - np.sum(y_observed - y_predicted)**2/\
-        np.sum(y_observed - np.mean(y_observed))**2
+    return 1 - np.sum((y_observed - y_predicted)**2)/\
+        np.sum((y_observed - np.mean(y_observed))**2)
 
 
-def create_design_matrix(x1, x2, deg, N):
+def create_design_matrix(x1, x2, N, deg):
     """
-    Construct a design matrix with N rows and deg+1 columns.
+    Construct a design matrix with N rows and features =
+    (deg + 1)*(deg + 2)/2 columns.  N is the number of samples and
+    features is the number of features of the design matrix.
 
     Parameters
     ----------
@@ -69,53 +68,65 @@ def create_design_matrix(x1, x2, deg, N):
     x2 : numpy.ndarray
         Dependent / outcome / response variable. Is it, though?
 
-    deg : int
-        The polynomial degree.
-
     N : int
         The number of data points.
+
+    deg : int
+        The polynomial degree.
 
     Returns
     -------
     X : numpy.ndarray
-        Design matrix of dimensions N rows and deg+1 columns.
+        Design matrix of dimensions N rows and (deg + 1)*(deg + 2)/2
+        columns.
     """
 
-    X = np.zeros((N, deg+1))
-    X[:, 0] = 1      # Intercept.
+    features = int((deg + 1)*(deg + 2)/2)
+    X = np.empty((N, features))     # Samples x features.
+    X[:, 0] = 1     # Intercept.
+    idx = 1         # For indexing the design matrix.
     
     for i in range(1, deg+1):
         """
-        Runs through all but the 0th column of X.
+        Runs through all polynomial degrees.
         """
         for j in range(i+1):
             """
             Runs through all combinations of x and y which produces an
             i'th degree polynomial.
             """
-            X[:, i] += (x1**j)*(x2**(i - j))  # THIS IS INCORRECT. EACH TERM SHOULD HAVE INDIVIDUAL COLUMNS!
-
+            X[:, idx] = (x1**j)*(x2**(i - j))
+            idx += 1
+    
     return X
-
-
 
 
 def solve():
     np.random.seed(1337)
-    N = 10      # Number of data ponts.
-    deg = 5     # Polynomial degree.
+    N = 100      # Number of data ponts.
+    deg = 3     # Polynomial degree.
     x1 = np.random.random(size=N)
     x2 = np.random.random(size=N)
 
-    y_observed = franke_function(x1, x2)
-    X = create_design_matrix(x1, x2, deg, N)
+    y_observed = franke_function(x1, x2)    # Is observed a good name?
+    X = create_design_matrix(x1, x2, N, deg)
 
-    # y_observed += stochastic_noise
-    # l = (degree + 2)*(degree + 1)/2
+    # y_observed += 0.1*np.random.randn(N)
 
-    # X_train, X_test, y_train, y_test = train_test_split()
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y_observed, test_size=0.2)
 
-    # beta = np.linalg.inv( )
+    beta = np.linalg.inv(X_train.T@X_train)@X_train.T@y_train
+
+    y_tilde = X_train@beta
+    y_predict = X_test@beta
+
+    print("train")
+    print(f"R^2: {r_squared(y_train, y_tilde)}")
+    print(f"MSE: {mean_squared_error(y_train, y_tilde)}")
+    print("test")
+    print(f"R^2: {r_squared(y_test, y_predict)}")
+    print(f"MSE: {mean_squared_error(y_test, y_predict)}")
 
 
 
