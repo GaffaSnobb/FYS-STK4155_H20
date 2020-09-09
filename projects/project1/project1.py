28f6bdfec54dc4d72237af9cc82ee144b4818cf3
jonkdahl <jon@dira.no>
2020-09-09 11:34:59 +0200

    The design matrix was not constructed correctly. Now all combinations of x and y (x1 and x2) are taken into account. Next step is to work on a vectorized solution.

--- projects/project1/project1.py
+++ projects/project1/project1.py
@@ -1,11 +1,12 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 def franke_function(x1, x2):
-    return 0.75*np.exp(-(0.25*(9*x1 - 2)**2) - 0.25*((9*x2 - 2)**2))\
-        + 0.75*np.exp(-((9*x1 + 1)**2)/49.0 - 0.1*(9*x2 + 1))\
-        + 0.5*np.exp(-(9*x1 - 7)**2/4.0 - 0.25*((9*x2 - 3)**2))\
+    return 0.75*np.exp(-(0.25*(9*x1 - 2)**2) - 0.25*((9*x2 - 2)**2)) \
+        + 0.75*np.exp(-((9*x1 + 1)**2)/49.0 - 0.1*(9*x2 + 1)) \
+        + 0.5*np.exp(-(9*x1 - 7)**2/4.0 - 0.25*((9*x2 - 3)**2)) \
         - 0.2*np.exp(-(9*x1 - 4)**2 - (9*x2 - 7)**2)
 
 
@@ -77,58 +78,105 @@ def create_design_matrix(x1, x2, N, deg):
     Returns
     -------
     X : numpy.ndarray
-        Design matrix of dimensions N rows and (deg + 1)*(deg + 2)/2
+        Design matrix of dimensions N**2 rows and (deg + 1)*(deg + 2)/2
         columns.
     """
 
-    features = int((deg + 1)*(deg + 2)/2)
-    X = np.empty((N, features))     # Samples x features.
-    X[:, 0] = 1     # Intercept.
-    idx = 1         # For indexing the design matrix.
+    x1 = x1.ravel()
+    x2 = x2.ravel()
     
-    for i in range(1, deg+1):
+    features = int((deg + 1)*(deg + 2)/2)
+    X = np.empty((N**2, features))     # Data points x features.
+    X[:, 0] = 1     # Intercept.
+
+    for i in range(N**2):
         """
-        Runs through all polynomial degrees.
+        Loop over all pairs of values in x1 and x2.
         """
-        for j in range(i+1):
+        col_idx = 1 # For indexing the design matrix columns.
+        for j in range(1, deg+1):
             """
-            Runs through all combinations of x and y which produces an
-            i'th degree polynomial.
+            Loop over all degrees except 0.
             """
-            X[:, idx] = (x1**j)*(x2**(i - j))
-            idx += 1
+            for k in range(j+1):
+                """
+                Loop over all combinations of x1 and x2 which produces
+                an j'th degree term.
+                """
+                X[i, col_idx] = (x1[i]**k)*(x2[i]**(j - k))
+                col_idx += 1
+
+    
+    # for i in range(1, deg+1):
+    #     """
+    #     Runs through all polynomial degrees.
+    #     """
+    #     for j in range(i+1):
+    #         """
+    #         Runs through all combinations of x and y which produces an
+    #         i'th degree polynomial.
+    #         """
+    #         X[:, idx] = (x1**j)*(x2**(i - j))
+    #         idx += 1
     
     return X
 
 
-def solve():
+def solve(debug=False):
     np.random.seed(1337)
-    N = 100      # Number of data ponts.
-    deg = 3     # Polynomial degree.
+    N = 3      # Number of data ponts.
+    deg = 2    # Polynomial degree.
     x1 = np.random.random(size=N)
     x2 = np.random.random(size=N)
+
+    x1, x2 = np.meshgrid(x1, x2)
 
     y_observed = franke_function(x1, x2)    # Is observed a good name?
     X = create_design_matrix(x1, x2, N, deg)
 
-    # y_observed += 0.1*np.random.randn(N)
+    print(X)
 
-    X_train, X_test, y_train, y_test = \
-        train_test_split(X, y_observed, test_size=0.2)
+    # # y_observed += 0.1*np.random.randn(N) # Stochastic noise.
 
-    beta = np.linalg.inv(X_train.T@X_train)@X_train.T@y_train
+    # X_train, X_test, y_train, y_test = \
+    #     train_test_split(X, y_observed, test_size=0.2)
 
-    y_tilde = X_train@beta
-    y_predict = X_test@beta
+    # print("x train")
+    # print(X_train)
+    # X_train = X_train.reshape(-1, 1)
+    # X_test = X_test.reshape(-1, 1)
 
-    print("train")
-    print(f"R^2: {r_squared(y_train, y_tilde)}")
-    print(f"MSE: {mean_squared_error(y_train, y_tilde)}")
-    print("test")
-    print(f"R^2: {r_squared(y_test, y_predict)}")
-    print(f"MSE: {mean_squared_error(y_test, y_predict)}")
+    # # HOW TO PROPERLY USE THE SCALING?
+    # scaler = StandardScaler()
+    # scaler.fit(X_train)     # Compute the mean and std for later scaling.
+    # X_train = scaler.transform(X_train)
+    # X_test = scaler.transform(X_test)
+    # print("x train")
+    # print(X_train)
 
+    # X_train = X_train.reshape(-1, 1)
+    # X_test = X_test.reshape(-1, 1)
 
+    # # beta = np.linalg.inv(X_train.T@X_train)@X_train.T@y_train
+    # beta = np.linalg.pinv(X_train.T@X_train)@X_train.T@y_train
+
+    # print("x")
+    # print(beta)
+
+    # y_tilde = X_train@beta
+    # y_predict = X_test@beta
+
+    # if debug:
+    #     print("train")
+    #     print(f"R^2: {r_squared(y_train, y_tilde)}")
+    #     print(f"MSE: {mean_squared_error(y_train, y_tilde)}")
+    #     print("test")
+    #     print(f"R^2: {r_squared(y_test, y_predict)}")
+    #     print(f"MSE: {mean_squared_error(y_test, y_predict)}")
+
+def compare():
+    pass
 
 if __name__ == "__main__":
-    solve()
+    solve(debug=True)
+    # compare()
