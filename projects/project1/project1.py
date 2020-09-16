e69e08b5fa45a04d35727be402818d75371fcf10
jonkdahl <jon@dira.no>
2020-09-16 17:51:41 +0200

    Create two classes, one for solve, one for compare. Variance-bias tradeoff seems to be working fine. The code is a bit messy, but the solution works.

--- projects/project1/project1.py
+++ projects/project1/project1.py
@@ -125,169 +125,253 @@ def create_design_matrix(x1, x2, N, deg):
     return X
 
 
-def solve(deg, N=34, noise_factor=0.15, debug_info=False, timing_info=False):
-    """
-    Solve the OLS on the Franke function.
+class Solve:
+    def __init__(self, deg, N=34, noise_factor=0.15, debug_info=False,
+        timing_info=False):
+        """
+        Solve the OLS on the Franke function.
 
-    Draw N numbers in the inverval [0, 1) for both variables, x1 and x2.
-    Make a meshgrid of x1 and x2 to ensure all combinations of x1 and x2
-    values.  Pass the meshgrids to the Franke function, ravel the
-    resulting array for easier calculations and add stochastic noise
-    drawn from the standard normal distribution. Create the design
-    matrix X based on the meshgrids, the number of randomly drawn points
-    and the polynomial degree.  Split the data into training and test
-    sets.  Scale the data by subtracting the mean and dividing by the
-    standard deviation.  Solve for the vector beta by matrix inversion
-    and matrix multiplication.  Use the beta vector to generate model
-    data (y_tilde) and predicted data (y_predict). Return the R^2 score
-    and MSE for both training and test data sets.
+        Draw N numbers in the inverval [0, 1) for both variables, x1 and x2.
+        Make a meshgrid of x1 and x2 to ensure all combinations of x1 and x2
+        values.  Pass the meshgrids to the Franke function, ravel the
+        resulting array for easier calculations and add stochastic noise
+        drawn from the standard normal distribution. Create the design
+        matrix X based on the meshgrids, the number of randomly drawn points
+        and the polynomial degree.  Split the data into training and test
+        sets.  Scale the data by subtracting the mean and dividing by the
+        standard deviation.
 
-    Parameters
-    ----------
-    deg : int
-        Polynomial degree.
+        Parameters
+        ----------
+        deg : int
+            Polynomial degree.
 
-    N : int
-        The number of data points per random variable.  The resulting
-        meshgrids will measure NxN values.
+        N : int
+            The number of data points per random variable.  The resulting
+            meshgrids will measure NxN values.
 
-    noise_factor : int, float
-        The factor of added stochastic noise.
+        noise_factor : int, float
+            The factor of added stochastic noise.
 
-    debug_info : boolean
-        For toggling print of debug data on / off.
+        debug_info : boolean
+            For toggling print of debug data on / off.
 
-    timing_info : boolean
-        For toggling print of timing info on / off.
+        timing_info : boolean
+            For toggling print of timing info on / off.
+        """
+        self.debug_info = debug_info
+        self.timing_info = timing_info
+        # x1, x2 = np.meshgrid(np.random.randn(N), np.random.randn(N))
+        x1, x2 = np.meshgrid(np.linspace(0, 1, N), np.linspace(0, 1, N))
 
-    Returns
-    -------
-    r_score_train : float
-        The R^2 value of the training set.
-    
-    mse_train : float
-        The mean squared error of the training set.
-
-    r_score_test : float
-        The R^2 value of the test set.
-
-    mse_test : float
-        The mean squared error of the test set.
-    """
-    # x1, x2 = np.meshgrid(np.random.randn(N), np.random.randn(N))
-    x1, x2 = np.meshgrid(np.linspace(0, 1, N), np.linspace(0, 1, N))
-
-    y_observed = franke_function(x1, x2).ravel()
-    y_observed += noise_factor*np.random.randn(N**2) # Stochastic noise.
-    
-    create_time = time.time()
-    X = create_design_matrix(x1, x2, N, deg)
-    create_time = time.time() - create_time
-
-    if timing_info:
-        print(f"design matrix created in {create_time:.3f} s")
-        print(f"design matrix dimensions {X.shape}")
-    
-    X_train, X_test, y_train, y_test = \
-        train_test_split(X, y_observed, test_size=0.2)
-
-    # Scaling.
-    X_mean = np.mean(X_train)
-    X_std = np.std(X_train)
-    X_train = (X_train - X_mean)/X_std
-    X_test = (X_test - X_mean)/X_std
-
-    betaa = np.linalg.pinv(X_train.T@X_train)@X_train.T@y_train
-
-    n_bootstraps = 50
-    Y_predict = np.empty((X_test.shape[0], n_bootstraps))
-    beta = np.empty((X_test.shape[1], n_bootstraps))
-
-    for i in range(n_bootstraps):
-        X_train_resample, y_train_resample = resample(X_train, y_train, replace=True)
-
-        inversion_time = time.time()
-        # beta = np.linalg.pinv(X_train_resample.T@X_train_resample)@X_train_resample.T@y_train_resample
-        beta[:, i] = np.linalg.pinv(X_train_resample.T@X_train_resample)@X_train_resample.T@y_train_resample
-        inversion_time = time.time() - inversion_time
-
-        if timing_info: print(f"solved for beta in {inversion_time:.3f} s")
-
-        Y_predict[:, i] = X_test@beta[:, i]
-        # Y_tilde[:, i] = X_train@beta[:, i]    # Should this be included?
-
-    y_tilde = X_train@(np.mean(beta, axis=1))
-    # y_tilde = X_train@betaa # THIS IS UNANSWERED
-    y_predict = np.mean(Y_predict, axis=1)
-
-    r_score_train = r_squared(y_train, y_tilde)
-    mse_train = mean_squared_error(y_train, y_tilde)
-    bias_train = bias(y_train, y_tilde)
-    variance_train = np.mean(np.var(Y_predict, axis=1))
-    
-    r_score_test = r_squared(y_test, y_predict)
-    mse_test = mean_squared_error(y_test, y_predict)
-    bias_test = bias(y_test, y_predict)
-
-    if debug_info:
-        print("train")
-        print(f"R^2: {r_score_train}")
-        print(f"MSE: {mse_train}")
-        print(f"bias: {bias_train}")
+        y_observed = franke_function(x1, x2).ravel()
+        y_observed += noise_factor*np.random.randn(N**2) # Stochastic noise.
         
-        print("\ntest")
-        print(f"R^2: {r_score_test}")
-        print(f"MSE: {mse_test}")
-        print(f"bias: {bias_test}")
+        create_time = time.time()
+        X = create_design_matrix(x1, x2, N, deg)
+        create_time = time.time() - create_time
 
-    return r_score_train, mse_train, r_score_test, mse_test
+        if self.timing_info:
+            print(f"design matrix created in {create_time:.3f} s")
+            print(f"design matrix dimensions {X.shape}")
+        
+        self.X_train, self.X_test, self.y_train, self.y_test = \
+            train_test_split(X, y_observed, test_size=0.2)
+
+        # Scaling.
+        X_mean = np.mean(self.X_train)
+        X_std = np.std(self.X_train)
+        self.X_train = (self.X_train - X_mean)/X_std
+        self.X_test = (self.X_test - X_mean)/X_std
 
 
-def compare(which="mse"):
-    """
-    Call 'solve' with a range of polynomial degrees and plot the R score
-    or mean squared error as a function of the polynomial degree.
+    def bootstrap(self, n_bootstraps=50):
+        """
+        Perform the OLS with bootstrapping.
 
-    Parameters
-    ----------
-    which : string
-        Choose whether to plot MSE or R score.  Allowed inputs are 'mse'
-        and 'r_score'.  Returns if any other argument is passed.
-    """
-    # np.random.seed(1337)
-    degrees = np.arange(1, 30+1, 1)
-    N_degrees = len(degrees)
-    N = 40  # The number of rows in X is N**2.
-    noise_factor = 0.15
-    
-    r_score_train = np.empty(N_degrees)
-    mse_train = np.empty(N_degrees)
-    r_score_test = np.empty(N_degrees)
-    mse_test = np.empty(N_degrees)
+        Parameters
+        ----------
+        n_bootstraps : int
+            The number of bootstrap samples.
 
-    for i in range(N_degrees):
-        r_score_train[i], mse_train[i], r_score_test[i], mse_test[i] = \
-            solve(degrees[i], N, noise_factor, debug_info=False, timing_info=True)
+        Returns
+        -------
+        r_score_train : float
+            The R^2 value of the training set.
+        
+        mse_train : float
+            The mean squared error of the training set.
+        """
+        Y_predict = np.empty((self.X_test.shape[0], n_bootstraps))
+        beta = np.empty((self.X_test.shape[1], n_bootstraps))
 
-    if which == "mse":
-        plt.semilogy(degrees, mse_train, label="mse_train")
-        plt.semilogy(degrees, mse_test, label="mse_test")
-        plt.ylabel("MSE")
+        for b in range(n_bootstraps):
+            X_train_resample, y_train_resample = resample(self.X_train,
+                self.y_train, replace=True)
 
-    elif which == "r_score":
-        plt.plot(degrees, r_score_train, label="r_score_train")
-        plt.plot(degrees, r_score_test, label="r_score_test")
-    
-    else:
-        print("Please choose 'mse' or 'r_score'.")
-        return
-    
-    plt.xlabel("ploynomial degree")
-    plt.legend()
-    plt.show()
-    
+            inversion_time = time.time()
+            beta[:, b] = np.linalg.pinv(X_train_resample.T@X_train_resample)@\
+                X_train_resample.T@y_train_resample
+            inversion_time = time.time() - inversion_time
+
+            if self.timing_info:
+                print(f"solved for beta in {inversion_time:.3f} s")
+
+            Y_predict[:, b] = self.X_test@beta[:, b]
+        
+        y_predict = np.mean(Y_predict, axis=1)
+        
+        r_score_test = r_squared(self.y_test, y_predict)
+        # mse_test = mean_squared_error(self.y_test, y_predict)
+        mse_test = np.mean( np.mean((self.y_test.reshape(-1, 1) - Y_predict)**2, axis=1) )
+        # bias_test = bias(self.y_test, y_predict)
+        
+        bias_test = np.mean( (self.y_test - np.mean(Y_predict, axis=1))**2 )
+        variance = np.mean(np.var(Y_predict, axis=1))
+
+        if self.debug_info:
+            print("train")
+            print(f"R^2: {r_score_test}")
+            print(f"MSE: {mse_test}")
+            print(f"bias: {bias_test}")
+
+        return r_score_test, mse_test, bias_test, variance
+
+
+    def no_bootstrap(self):
+        """
+        Perform the OLS with no bootstrapping.
+
+        Solve for the vector beta by matrix inversion and matrix
+        multiplication.  Use the beta vector to generate model data
+        (y_tilde) and predicted data (y_predict). Return the R^2 score
+        and MSE for both training and test data sets.
+
+        Returns
+        -------
+        r_score_train : float
+            The R^2 value of the training set.
+        
+        mse_train : float
+            The mean squared error of the training set.
+
+        r_score_test : float
+            The R^2 value of the test set.
+
+        mse_test : float
+            The mean squared error of the test set.
+        """
+        
+        inversion_time = time.time()
+        beta = np.linalg.pinv(self.X_train.T@self.X_train)@self.X_train.T@self.y_train
+        inversion_time = time.time() - inversion_time
+        if self.timing_info:
+            print(f"solved for beta in {inversion_time:.3f} s")
+
+        y_tilde = self.X_train@beta
+        y_predict = self.X_test@beta
+
+        r_score_train = r_squared(self.y_train, y_tilde)
+        mse_train = mean_squared_error(self.y_train, y_tilde)
+        r_score_test = r_squared(self.y_test, y_predict)
+        mse_test = mean_squared_error(self.y_test, y_predict)
+
+        if self.debug_info:
+            print("\ntrain")
+            print(f"R^2: {r_score_train}")
+            print(f"MSE: {mse_train}")
+            
+            print("train (sklearn)")
+            print(f"R^2: {skl.r2_score(y_train, y_tilde)}")
+            print(f"MSE: {skl.mean_squared_error(y_train, y_tilde)}")
+            
+            print("\ntest")
+            print(f"R^2: {r_score_test}")
+            print(f"MSE: {mse_test}")
+            
+            print("test (sklearn)")
+            print(f"R^2: {skl.r2_score(y_test, y_predict)}")
+            print(f"MSE: {skl.mean_squared_error(y_test, y_predict)}")
+
+        return r_score_train, mse_train, r_score_test, mse_test
+
+
+class Compare:
+
+    def __init__(self, max_degree, N, noise_factor):
+        self.degrees = np.arange(1, max_degree+1, 1)
+        self.N_degrees = len(self.degrees)
+        self.N = N  # The number of rows in X is N**2.
+        self.noise_factor = noise_factor
+
+
+    def compare_no_bootstrap(self, which="mse"):
+        """
+        Use 'Solve' with a range of polynomial degrees and plot the R score
+        or mean squared error as a function of the polynomial degree.
+
+        Parameters
+        ----------
+        which : string
+            Choose whether to plot MSE or R score.  Allowed inputs are 'mse'
+            and 'r_score'.  Returns if any other argument is passed.
+        """
+
+        r_score_train = np.empty(self.N_degrees)
+        mse_train = np.empty(self.N_degrees)
+        r_score_test = np.empty(self.N_degrees)
+        mse_test = np.empty(self.N_degrees)
+
+        for i in range(self.N_degrees):
+            q = Solve(self.degrees[i], self.N, self.noise_factor,
+                debug_info=False, timing_info=True)
+            r_score_train[i], mse_train[i], r_score_test[i], mse_test[i] =\
+                q.no_bootstrap()
+
+
+        if which == "mse":
+            plt.semilogy(self.degrees, mse_train, label="mse_train")
+            plt.semilogy(self.degrees, mse_test, label="mse_test")
+            plt.ylabel("MSE")
+
+        elif which == "r_score":
+            plt.plot(self.degrees, r_score_train, label="r_score_train")
+            plt.plot(self.degrees, r_score_test, label="r_score_test")
+        
+        else:
+            print("Please choose 'mse' or 'r_score'.")
+            return
+        
+        plt.xlabel("ploynomial degree")
+        plt.legend()
+        plt.show()
+
+
+    def compare_bootstrap(self, n_bootstraps):
+        """
+
+        """
+        r_score = np.empty(self.N_degrees)
+        mse = np.empty(self.N_degrees)
+        bias = np.empty(self.N_degrees)
+        variance = np.empty(self.N_degrees)
+        
+        for i in range(self.N_degrees):
+            q = Solve(self.degrees[i], self.N, self.noise_factor,
+                debug_info=False, timing_info=True)
+            r_score[i], mse[i], bias[i], variance[i] =\
+                q.bootstrap(n_bootstraps)
+
+        plt.semilogy(self.degrees, variance, label="variance")
+        plt.semilogy(self.degrees, bias, label="bias")
+        plt.semilogy(self.degrees, mse, label="mse")
+        # plt.plot(self.degrees, variance+bias, label="var+bias")
+        plt.legend()
+        plt.show()
+        
 
 if __name__ == "__main__":
     # np.random.seed(1337)
-    # solve(deg=2, N=3, debug_info=True)
-    compare(which='mse')
+    q = Compare(max_degree=15, N=20, noise_factor=0.1)
+    # q.compare_no_bootstrap(which='mse')
+    q.compare_bootstrap(n_bootstraps=20)
