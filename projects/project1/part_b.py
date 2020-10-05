6c675417b5df516bde53699c347452aa49059f92
jonkdahl <jon@dira.no>
2020-10-05 15:42:32 +0200

    Some changes to the plot layout.

--- projects/project1/part_b.py
+++ projects/project1/part_b.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     max_poly_degree = 10
     noise_factor = 0.1
     n_bootstraps = 50
-    repetitions = 10 # Redo the experiment and average the data.
+    repetitions = 100 # Redo the experiment and average the data.
     
     degrees = np.arange(1, max_poly_degree+1, 1)
     n_degrees = len(degrees)
@@ -16,11 +16,15 @@ if __name__ == "__main__":
     bias = np.zeros(n_degrees)
     variance = np.zeros(n_degrees)
 
-    fig, axs = plt.subplots(2,2)
-    fig.suptitle("Bootstrap", fontsize=17)
+    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(9, 7))
+    # fig.suptitle("Bootstrap", fontsize=17)
+    fig.tight_layout(pad=3)
+    fig.text(x=0.4, y=0.02, s="Polynomial degree", fontsize=15)
+    fig.text(x=0.01, y=0.48, s="Error", fontsize=15, rotation="vertical")
     axs = axs.ravel()
     
     for k in range(len(n_data_points)):
+        print(f"data points {k+1} of {len(n_data_points)}, {n_data_points[k]}")
         for i in range(repetitions):
             """
             Repeat the experiment and average the produced values.
@@ -44,8 +48,15 @@ if __name__ == "__main__":
         axs[k].plot(degrees, variance, label="variance", color="grey", linestyle="dashed")
         axs[k].plot(degrees, bias, label="bias", color="black", linestyle="dotted")
         axs[k].set_title("Data points %d" %n_data_points[k])
-        axs[k].set(xlabel="Polynomial degree", ylabel="Error")
-        axs[k].label_outer()
+        axs[k].tick_params(labelsize=12)
+        axs[k].set_ylim(-0.01, 0.07)
+        # axs[k].set(xlabel="Polynomial degree", ylabel="Error")
+        # axs[k].label_outer()
         #axs[k].tick_params(labelsize=12)
-    plt.legend(fontsize=12)
-    plt.show()
+    
+    axs[1].set_yticklabels(labels=[])
+    axs[3].set_yticklabels(labels=[])
+    axs[0].set_xticklabels(labels=[])
+    axs[1].set_xticklabels(labels=[])
+    axs[1].legend(fontsize=12, loc="upper right")
+    fig.savefig(dpi=300, fname="part_b_bootstrap_bias_variance_tradeoff.pdf")
