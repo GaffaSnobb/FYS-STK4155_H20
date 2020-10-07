7a8c65f50e1fcff85eca268f85312121bc5d4e01
jonkdahl <jon@dira.no>
2020-10-07 14:53:44 +0200

    Update plot.

--- projects/project1/part_d.py
+++ projects/project1/part_d.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 from common import Regression
 
 def bootstrap():
@@ -8,12 +9,12 @@ def bootstrap():
     max_poly_degree = 10
     noise_factor = 0.2
     n_bootstraps = 50
-    repetitions = 1 # Redo the experiment and average the data.
+    repetitions = 5 # Redo the experiment and average the data.
 
     degrees = np.arange(1, max_poly_degree+1, 1)
     n_degrees = len(degrees)
 
-    n_lambdas = 20
+    n_lambdas = 30
     # lambdas = np.linspace(-2, 1, n_lambdas)
     lambdas = np.logspace(-8, -2, n_lambdas)
 
@@ -53,29 +54,30 @@ def bootstrap():
     X, Y = np.meshgrid(degrees, lambdas)
     
 
-    fig1, ax0 = plt.subplots(nrows=1, ncols=2)
+    fig1, ax0 = plt.subplots(nrows=2, ncols=1, figsize=(8, 9))
+    fig1.text(x=0.87, y=0.44, s=r"$log_{10}(error)$", rotation="90", fontsize=15)
+    fig1.text(x=0.02, y=0.52, s=r"$\lambda$", rotation="90", fontsize=15)
 
     ax0 = ax0.ravel()
     mappable = ax0[0].contourf(X, Y, np.log10(variance_boot))
     ax0[0].set_title("Variance", fontsize=17)
-    ax0[0].set_xlabel("Degrees", fontsize=15)
-    ax0[0].set_ylabel("Lambdas", fontsize=15)
     ax0[0].tick_params(labelsize=12)
-    cbar = plt.colorbar(mappable, ax=ax0[0])
+    cbar = plt.colorbar(mappable, ax=ax0[0], format=r"%.1f")
     cbar.ax.tick_params(labelsize=12)
+    # cbar.ax.set_major_formatter(FormatStrFormatter('%.2f'))
     # cbar.set_label(r"$log_{10}$ error", fontsize=40)
     # cbar.ax.tick_params(labelsize=30)
     # plt.show()
 
     mappable = ax0[1].contourf(X, Y, np.log10(bias_boot))
     ax0[1].set_title("Bias", fontsize=17)
-    ax0[1].set_xlabel("Degrees", fontsize=15)
-    ax0[1].set_ylabel("Lambdas", fontsize=15)
+    ax0[1].set_xlabel("Degree", fontsize=15)
     ax0[1].tick_params(labelsize=12)
-    cbar = plt.colorbar(mappable)
+    cbar = plt.colorbar(mappable, format=r"%.1f")
     cbar.ax.tick_params(labelsize=12)
     #cbar.set_label(r"$log_{10}$ error", fontsize=40)
     # cbar.ax.tick_params(labelsize=30)
+    plt.savefig(dpi=300, fname="part_d_ridge_bootstrap_variance_bias.png")
     plt.show()
 
     # fig1, ax1 = plt.subplots()
@@ -89,17 +91,17 @@ def bootstrap():
 
 
 def cross_validation():
-    n_data_points = 400
+    n_data_points = 800
     max_poly_degree = 10
     noise_factor = 0.2
     folds = 5
-    repetitions = 10 # Redo the experiment and average the data.
+    repetitions = 100 # Redo the experiment and average the data.
 
     degrees = np.arange(1, max_poly_degree+1, 1)
     n_degrees = len(degrees)
 
-    n_lambdas = 40
-    lambdas = np.logspace(-8, -2, n_lambdas)
+    n_lambdas = 100
+    lambdas = np.logspace(-12, -4, n_lambdas)
     # lambdas = np.linspace(-1, 1, n_lambdas)
     mse_cv = np.zeros((n_lambdas, n_degrees))
     
@@ -128,15 +130,23 @@ def cross_validation():
     mse_cv /= repetitions
     idx = np.unravel_index(np.argmin(mse_cv), mse_cv.shape)
     X, Y = np.meshgrid(degrees, lambdas)
-    plt.contourf(X, Y, np.log10(mse_cv))
-    plt.xlabel("Degrees", fontsize=15)
-    plt.ylabel("Lambdas",fontsize=15)
-    plt.title(f"min: lambda={lambdas[idx[0]]}, degree={degrees[idx[1]]}", fontsize=15)
-    plt.colorbar()
+
+    fig, ax = plt.subplots(figsize=(9, 7))
+
+    mappable = ax.contourf(X, Y, mse_cv)
+    ax.set_xlabel("Polynomial degree", fontsize=15)
+    ax.set_ylabel(r"$\lambda$",fontsize=15)
+    ax.set_title(f"min(MSE) = {np.amin(mse_cv):.4e}: $\lambda$={lambdas[idx[0]]:.4e}, degree={degrees[idx[1]]}", fontsize=15)
+    ax.plot(degrees[idx[1]], lambdas[idx[0]], "ro")
+    ax.tick_params(labelsize=12)
+    cbar = plt.colorbar(mappable, format=r"%.3f")
+    cbar.ax.tick_params(labelsize=12)
+    cbar.set_label(r"MSE", fontsize=15)
+    plt.savefig(dpi=300, fname="part_d_ridge_cv_mse.png")
     plt.show()
 
 
 if __name__ == "__main__":
-    bootstrap()
+    # bootstrap()
     cross_validation()
     pass
