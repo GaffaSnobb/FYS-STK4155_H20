9e6fe26a3bf297ddaccfd04421c692c7382f500c
jonkdahl <jon@dira.no>
2020-10-22 15:32:16 +0200

    Update graphical visualisation to include MSE.

--- projects/project2/src/task_a.py
+++ projects/project2/src/task_a.py
@@ -30,28 +30,6 @@ class Example1D(common._StatTools):
         super(Example1D, self).__init__(n_data_total, poly_degree, init_beta)
 
 
-    def show(self):
-        """
-        Plot and show the fit data and compare with
-        scipy.optimize.curve_fit.
-        """
-        n_scope = 1000
-        scope = np.linspace(0, 1, n_scope)
-        res = common.polynomial_1d(scope, *self.beta)
-
-        popt, pcov = curve_fit(f=common.polynomial_1d, xdata=self.x1,
-            ydata=self.y, p0=[0]*(self.poly_degree+1))
-
-        print(f"curve_fit: {popt}")
-        print(f"gd: {self.beta}")
-
-        plt.plot(self.x1, self.y, "r.")
-        plt.plot(scope, res, label="gradient descent")
-        plt.plot(scope, common.polynomial_1d(scope, *popt), label="curve_fit")
-        plt.legend()
-        plt.show()
-
-
 class Example2D(common._StatTools):
     def __init__(self, n_data_total, poly_degree):
         """
@@ -79,31 +57,50 @@ class Example2D(common._StatTools):
 
 
 
+def visualize_fit_1d():
+    """
+    Perform a fit on data of 1 dependent variable.  Plot SGD fit with
+    scipy.optimize.curve_fit for comparison.
+    """
+    n_scope = 1000
+    scope = np.linspace(0, 1, n_scope)
+    
+    q = Example1D(n_data_total=200, poly_degree=3)
+    q.gradient_descent(iterations=1000, step_size=0.3)
+    
+    gd_res = common.polynomial_1d(scope, *q.beta)
+    gd_mse = common.mean_squared_error(q.y_train, q.X_train@q.beta)
+    q.reset_beta()
+    
+    q.stochastic_gradient_descent(n_epochs=100, n_batches=20, lambd=0)
+    sgd_res = common.polynomial_1d(scope, *q.beta)
+    sgd_mse = common.mean_squared_error(q.y_train, q.X_train@q.beta)
+
+    # q.x1 = (q.x1 - q.X_mean)/q.X_std  # Scaling is not working yet.
+
+    popt, _ = curve_fit(f=common.polynomial_1d, xdata=q.X_train[:, 1],
+        ydata=q.y_train, p0=[0]*(q.poly_degree+1))
+
+    curve_fit_mse = common.mean_squared_error(q.y_train,
+        common.polynomial_1d(q.X_train[:, 1], *popt))
+
+
+    plt.plot(q.x1, q.y, "r.")
+    plt.plot(scope, gd_res, label=f"GD, MSE: {gd_mse:.4f}")
+    plt.plot(scope, sgd_res, label=f"SGD, MSE: {sgd_mse:.4f}")
+    plt.plot(scope, common.polynomial_1d(scope, *popt),
+        label=f"scipy.curve_fit, MSE: {curve_fit_mse:.4f}")
+    plt.legend()
+    plt.show()
+
+def compare_1d():
+    q = Example1D(n_data_total=200, poly_degree=3)
+    q.stochastic_gradient_descent(n_epochs=100, n_batches=20, lambd=0)
 
 if __name__ == "__main__":
     # q = Example1D(n_data_total=100, poly_degree=3)
     # q.gradient_descent(iterations=1000, step_size=0.3)
     # q.show()
-
-    q = Example1D(n_data_total=200, poly_degree=3)
-    q.stochastic_gradient_descent(n_epochs=100, n_batches=20, lambd=0)
-
-
-    n_scope = 1000
-    scope = np.linspace(0, 1, n_scope)
-    res = common.polynomial_1d(scope, *q.beta)
-    # q.x1 = (q.x1 - q.X_mean)/q.X_std
-
-    popt, pcov = curve_fit(f=common.polynomial_1d, xdata=q.x1,
-        ydata=q.y, p0=[0]*(q.poly_degree+1))
-
-    print(f"curve_fit: {popt}")
-    print(f"gd: {q.beta}")
-
-    plt.title("with scaling")
-    plt.plot(q.x1, q.y, "r.")
-    plt.plot(scope, res, label="SGD")
-    plt.plot(scope, common.polynomial_1d(scope, *popt), label="scipy.curve_fit")
-    plt.legend()
-    plt.show()
+    # compare_1d()
+    visualize_fit_1d()
     pass
