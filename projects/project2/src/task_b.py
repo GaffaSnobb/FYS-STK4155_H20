6e2753c5eb44c6cc826b19c529f8ded112221cf8
jonkdahl <jon@dira.no>
2020-11-04 16:19:18 +0100

    FFNN seems to be working reasonably well.

--- projects/project2/src/task_b.py
+++ projects/project2/src/task_b.py
@@ -21,37 +21,16 @@ class FFNN(_StatTools):
         digits = datasets.load_digits()
         self.X = digits.images
         self.y = digits.target
-        print(self.X.shape)
         self.n_data_total = self.X.shape[0] # Total number of data points.
         self.X = self.X.reshape(self.n_data_total, -1)
-        print(self.X.shape)
         
         self.n_features = self.X.shape[1]   # The number of features.
         self.n_hidden_neurons = 50
         self.epochs = 50
         self.batch_size = 20        # Size of each minibatch.
         self.n_categories = 10      # Number of output categories. 0, 1, 2, ...
-        self.learning_rate = 0.1    # Aka eta.
-        self.lambd = 0
-
-        self.X_train, self.X_test, self.y_train, self.y_test = \
-            train_test_split(self.X, self.y, test_size=0.2, shuffle=True)
-
-        print(f"y_train: {self.y_train.shape}")
-
-        self.y_train = to_categorical(self.y_train)
-        self.y_test = to_categorical(self.y_test)
-
-        print(f"y_train: {self.y_train.shape}")
-
-        
-        # Weights and biases for the hidden layers.
-        self.hidden_weights = np.random.randn(self.n_features, self.n_hidden_neurons)
-        self.hidden_biases = np.zeros(self.n_hidden_neurons) + 0.01
-
-        # Weights and biases for the output layer.
-        self.output_weights = np.random.randn(self.n_hidden_neurons, self.n_categories)
-        self.output_biases = np.zeros(self.n_categories) + 0.01
+        # self.learning_rate = 0.1    # Aka eta.
+        # self.lambd = 0
 
 
     def feedforward(self):
@@ -65,7 +44,7 @@ class FFNN(_StatTools):
         self.probabilities = z_output/np.sum(z_output, axis=1, keepdims=True)
 
     
-    def backpropagation(self):
+    def _backpropagation(self):
         error_output = self.probabilities - self.y_minibatch    # Loss.
         error_hidden = error_output@self.output_weights.T*self.a_hidden*(1 - self.a_hidden) # Hard coded Sigmoid derivative?
 
@@ -88,13 +67,34 @@ class FFNN(_StatTools):
         self.hidden_biases -= self.learning_rate*hidden_biases_gradient
 
 
-    def train(self):
+    def _initial_state(self):
+        """
+        Set the system to the correct state before training starts.
+        """
+        self.X_train, self.X_test, self.y_train, self.y_test = \
+            train_test_split(self.X, self.y, test_size=0.2, shuffle=True)
+        self.y_train = to_categorical(self.y_train)
+
+        # Weights and biases for the hidden layers.
+        self.hidden_weights = np.random.randn(self.n_features, self.n_hidden_neurons)
+        self.hidden_biases = np.zeros(self.n_hidden_neurons) + 0.01
+
+        # Weights and biases for the output layer.
+        self.output_weights = np.random.randn(self.n_hidden_neurons, self.n_categories)
+        self.output_biases = np.zeros(self.n_categories) + 0.01
+
+
+    def train_neural_network(self, learning_rate=0.1, lambd=0):
         """
         Train the neural network.
         """
+        self._initial_state()
+        self.start_timing()
+        self.learning_rate = learning_rate
+        self.lambd = lambd
+        
         data_indices = np.arange(self.X_train.shape[0])
         iterations = self.n_data_total//self.batch_size
-        print(f"iterations: {iterations}")
 
         for _ in range(self.epochs):
             """
@@ -112,30 +112,30 @@ class FFNN(_StatTools):
                 self.y_minibatch = self.y_train[minibatch_indices]
 
                 self.feedforward()
-                self.backpropagation()
+                self._backpropagation()
+
+        self.stop_timing()
 
 
     def predict(self, X):
         self.X_minibatch = X
         self.feedforward()
-        print(np.argmax(self.probabilities, axis=1))
-        print(self.probabilities.shape)
-        print(np.argmax(self.y_test, axis=1))
-        print(self.y_test.shape)
-        # print(np.argmax(self.probabilities, axis=1).shape)
-        # print(self.probabilities)
+        # print(np.argmax(self.probabilities, axis=1))
         # print(self.probabilities.shape)
-        print(accuracy_score(np.argmax(self.probabilities, axis=1), np.argmax(self.y_test, axis=1)))
-        # print(len(self.probabilities))
-        # print(q.X_train.shape)
-
-
-# def predict(X):
-#     probabilities = feed_forward(X)
-#     return np.argmax(probabilities, axis=1)
+        # print(np.argmax(self.y_test, axis=1))
+        # print(self.y_test.shape)
+        score = accuracy_score(np.argmax(self.probabilities, axis=1), self.y_test)
+        # print(f"score: {score}")
+        return score
 
 if __name__ == "__main__":
     np.random.seed(1337)
     q = FFNN()
-    q.train()
-    q.predict(q.X_test)
+
+    learning_rates = np.logspace(-5, 0, 8)
+
+    for learning_rate in learning_rates:
+
+        q.train_neural_network(learning_rate)
+        score = q.predict(q.X_test)
+        print(f"score: {score} for learning rate: {learning_rate}")
