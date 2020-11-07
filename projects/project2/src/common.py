8bc78a58a67a03ec4c6f1f0f99b10aaf20bfde66
jonkdahl <jon@dira.no>
2020-11-07 17:49:30 +0100

    Add categorical function. Copied from tensorflow to reduce import time.

--- projects/project2/src/common.py
+++ projects/project2/src/common.py
@@ -159,8 +159,55 @@ def create_design_matrix_one_dependent_variable(x, n_data_total, poly_degree):
     return X
 
 
+def to_categorical(y, num_classes=None, dtype='float32'):
+    """
+    This function is copied from from tensorflow.keras.utils to reduce
+    import time.
+
+    Convert a class vector (integers) to binary class matrix.
+
+    E.g. for use with categorical_crossentropy.
+
+    Parameters
+    ----------
+    y : numpy.ndarray
+        Class vector to be converted into a matrix (integers from 0 to
+        num_classes).
+    
+    num_classes : NoneType, int 
+        Total number of classes. If `None`, this would be inferred as
+        the (largest number in `y`) + 1.
+    
+    dtype : str
+        The data type expected by the input. Default: `'float32'`.
+
+    Returns
+    -------
+    A binary matrix representation of the input. The classes axis is
+    placed last.
+
+    Raises
+    ------
+    ValueError: If input contains string value.
+    """
+    
+    y = np.array(y, dtype='int')
+    input_shape = y.shape
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+    y = y.ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes), dtype=dtype)
+    categorical[np.arange(n), y] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
+
+
 class _StatTools:
-    def __init__(self, n_data_total, poly_degree, init_beta=None, verbose=False):
+    def __init__(self, n_data_total, poly_degree, init_beta=None):
         """
         Parameters
         ----------
@@ -172,15 +219,11 @@ class _StatTools:
 
         init_beta : NoneType, numpy.ndarray
             Initial beta values.  Defaults to None where 0 is used.
-
-        verbose : bool
-            Toggle verbose mode on / off.
         """
         self.n_data_total = n_data_total
         self.poly_degree = poly_degree
         self.n_features = features(self.poly_degree, self.n_dependent_variables)
         self.init_beta = init_beta
-        self.verbose = verbose
 
         self._split_scale()
 
