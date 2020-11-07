a863217be63053b431139e8a715399ef6fbf2cf4
jonkdahl <jon@dira.no>
2020-11-07 23:34:46 +0100

    Add extensive testing of feedforward and backpropagation steps. Test by tolerance instead of absolute equality.

--- projects/project2/src/test.py
+++ projects/project2/src/test.py
@@ -28,18 +28,6 @@ class Example2D(common._StatTools):
         super(Example2D, self).__init__(n_data_total, poly_degree, init_beta)
 
 
-def test_design_matrix_dimensions():
-    q = Example2D(2, 5)
-
-    expected = (2, 21)
-    success = expected == q.X.shape
-    msg = "Design matrix dimensions do not match."
-    msg += f" Expected: {expected} got {q.X.shape}."
-
-    assert success, msg
-
-
-
 class FFNNSingle(FFNN):
     def predict_single(self, X):
         self.X_minibatch = X
@@ -52,34 +40,36 @@ class FFNNSingle(FFNN):
 
     def _initial_state_single(self):
         """
-        Remove when multilayer works.
+        Set the system to the correct state before training starts.
+        Split the data into training and testing sets.  Initialize the
+        weights and biases for the hidden layer and the output layer.
         """
-
         self.X_train, self.X_test, self.y_train, self.y_test = \
             train_test_split(self.X, self.y, test_size=0.2, shuffle=True)
         self.y_train = common.to_categorical(self.y_train)
 
         # Weights and biases for the hidden layers.
-        self.hidden_weights = np.random.randn(self.n_features, self.n_hidden_neurons)
-        self.hidden_biases = np.zeros(self.n_hidden_neurons) + 0.01
+        self.hidden_weights = np.random.normal(size=(self.n_features, self.n_hidden_neurons))
+        self.hidden_biases = np.full(shape=self.n_hidden_neurons, fill_value=0.01)
 
         # Weights and biases for the output layer.
-        self.output_weights = np.random.randn(self.n_hidden_neurons, self.n_categories)
-        self.output_biases = np.zeros(self.n_categories) + 0.01
+        self.output_weights = np.random.normal(size=(self.n_hidden_neurons, self.n_categories))
+        self.output_biases = np.full(shape=self.n_categories, fill_value=0.01)
 
 
     def _backpropagation_single(self):
         """
-        Remove when multilayer works.
+
         """
-        error_output = self.probabilities - self.y_minibatch    # Loss.
-        error_hidden = error_output@self.output_weights.T*self.a_hidden*(1 - self.a_hidden) # Hard coded Sigmoid derivative?
+        self.output_error = self.probabilities - self.y_minibatch    # Loss.
+        self.hidden_error = self.output_error@self.output_weights.T
+        self.hidden_error *= self.a_hidden*(1 - self.a_hidden) # Hard coded Sigmoid derivative.
 
-        output_weights_gradient = self.a_hidden.T@error_output
-        output_bias_gradient = np.sum(error_output, axis=0)
+        output_weights_gradient = self.a_hidden.T@self.output_error
+        self.output_bias_gradient = np.sum(self.output_error, axis=0)
 
-        hidden_weights_gradient = self.X_minibatch.T@error_hidden
-        hidden_biases_gradient = np.sum(error_hidden, axis=0)
+        hidden_weights_gradient = self.X_minibatch.T@self.hidden_error
+        hidden_biases_gradient = np.sum(self.hidden_error, axis=0)
 
         if self.lambd > 0:
             """
@@ -89,14 +79,14 @@ class FFNNSingle(FFNN):
             hidden_weights_gradient += self.lambd*self.hidden_weights
 
         self.output_weights -= self.learning_rate*output_weights_gradient
-        self.output_biases -= self.learning_rate*output_bias_gradient
+        self.output_biases -= self.learning_rate*self.output_bias_gradient
         self.hidden_weights -= self.learning_rate*hidden_weights_gradient
         self.hidden_biases -= self.learning_rate*hidden_biases_gradient
 
 
     def feedforward_single(self):
         """
-        Remove when multilayer works.
+        Perform one feedforward.
         """
         self.a_hidden = sigmoid(self.X_minibatch@self.hidden_weights + self.hidden_biases)
         self.z_output = np.exp(self.a_hidden@self.output_weights + self.output_biases)
@@ -138,64 +128,173 @@ class FFNNSingle(FFNN):
         if self.verbose: self.stop_timing()
 
 
-def test_multilayer_with_one_layer():
-    np.random.seed(1337)
-    q1 = FFNN()
-    q1._initial_state()
-    q1.X_minibatch = q1.X_test
-    q1.feedforward()
+def test_design_matrix_dimensions():
+    q = Example2D(2, 5)
 
-    # q1.train_neural_network()
-    # score = q1.predict(q1.X_test)
-    # print(score)
+    expected = (2, 21)
+    success = expected == q.X.shape
+    msg = "Design matrix dimensions do not match."
+    msg += f" Expected: {expected} got {q.X.shape}."
 
-    np.random.seed(1337)
-    q2 = FFNNSingle()
-    q2._initial_state_single()
-    q2.X_minibatch = q1.X_test
-    q2.feedforward_single()
+    assert success, msg
 
+tol = 1e-10
+
+np.random.seed(1337)
+q1 = FFNN()
+q1._initial_state()
+q1.X_minibatch = q1.X_train
+q1.feedforward()
+
+np.random.seed(1337)
+q2 = FFNNSingle()
+q2._initial_state_single()
+q2.X_minibatch = q2.X_train
+q2.feedforward_single()
+
+
+def test_initial_state_and_feedforward_output_weights():
     for i in range(50):
         for j in range(10):
-            if q2.output_weights[i, j] != q1.output_weights[i, j]:
+            if np.abs(q2.output_weights[i, j] - q1.output_weights[i, j]) > tol:
                 msg = f"output weight error at row: {i} col: {j}"
                 assert False, msg
 
+
+def test_initial_state_and_feedforward_output_biases():
     for i in range(10):
-        if q2.output_biases[i] != q1.output_biases[i]:
+        if np.abs(q2.output_biases[i] - q1.output_biases[i]) > tol:
             msg = f"output bias error at index: {i}"
             assert False, msg
 
+
+def test_initial_state_and_feedforward_hidden_weights():
     for i in range(64):
         for j in range(50):
-            if q2.hidden_weights[i, j] != q1.hidden_weights[0][i, j]:
+            if np.abs(q2.hidden_weights[i, j] - q1.hidden_weights[0][i, j]) > tol:
                 msg = f"hidden weight error at row: {i} col: {j}"
                 assert False, msg
 
+
+def test_initial_state_and_feedforward_hidden_biases():
     for i in range(50):
-        if q2.hidden_biases[i] != q1.hidden_biases[0][i]:
+        if np.abs(q2.hidden_biases[i] - q1.hidden_biases[0][i]) > tol:
             msg = f"hidden bias error at index: {i}"
             assert False, msg
 
-    for i in range(50):
-        for j in range(10):
-            if q2.output_weights[i, j] != q1.output_weights[i, j]:
-                msg = f"output weight error at row: {i} col: {j}"
-                assert False, msg
-            
+
+def test_initial_state_and_feedforward_hidden_neuron_input():
     for i in range(360):
         for j in range(50):
-            if q2.a_hidden[i, j] != q1.neuron_input[1][i, j]:
+            if np.abs(q2.a_hidden[i, j] - q1.neuron_input[1][i, j]) > tol:
                 msg = f"neuron input error at row: {i} col: {j}"
                 assert False, msg
 
+
+def test_initial_state_and_feedforward_output_neuron_activation():
     for i in range(360):
         for j in range(10):
-            if q2.z_output[i, j] != q1.neuron_activation[-1][i, j]:
+            if np.abs(q2.z_output[i, j] - q1.neuron_activation[-1][i, j]) > tol:
                 msg = f"neuron activation error at row: {i} col: {j}"
+                assert False, msg
+
+
+np.random.seed(1337)
+q3 = FFNN()
+q3._initial_state()
+q3.X_minibatch = q3.X_train
+q3.y_minibatch = q3.y_train
+q3.learning_rate = 0.1
+q3.lambd = 0
+q3.feedforward()
+q3._backpropagation()
+
+np.random.seed(1337)
+q4 = FFNNSingle()
+q4._initial_state_single()
+q4.X_minibatch = q4.X_train
+q4.y_minibatch = q4.y_train
+q4.learning_rate = 0.1
+q4.lambd = 0
+q4.feedforward_single()
+q4._backpropagation_single()
+
+def test_backpropagation_probabilities():
+    for i in range(1437):
+        for j in range(10):
+            if np.abs(q4.probabilities[i, j] - q3.probabilities[i, j]) > tol:
+                msg = f"probability error at row: {i} col: {j}"
+                assert False, msg
+
+
+def test_backpropagation_output_error():
+    for i in range(1437):
+        for j in range(10):
+            if np.abs(q4.output_error[i, j] - q3.error[-1][i, j]) > tol:
+                msg = f"error output error at row: {i} col: {j}"
+                assert False, msg
+
+
+def test_backpropagation_output_bias_gradient():
+    for i in range(10):
+        if np.abs(q4.output_bias_gradient[i] - q3.bias_gradient[-1][i]) > tol:
+            msg = f"output bias gradient error at index {i}"
+            assert False, msg
+
+
+def test_backpropagation_output_biases():
+    for i in range(10):
+        if np.abs(q4.output_biases[i] - q3.output_biases[i]) > tol:
+            msg = f"output bias error at index {i}"
+            assert False, msg
+
+
+def test_backpropagation_hidden_error():
+    q3_hidden_error = q3.error[-2].T    # Transpose only once.
+    for i in range(1437):
+        for j in range(50):
+            if np.abs(q4.hidden_error[i, j] - q3_hidden_error[i, j]) > tol:
+                msg = f"hidden error error at row: {i} col: {j}"
+                assert False, msg
+
+
+def test_backpropagation_output_weights():
+    for i in range(50):
+        for j in range(10):
+            if np.abs(q4.output_weights[i, j] - q3.output_weights[i, j]) > tol:
+                msg = f"output weights error at row: {i} col: {j}"
+                assert False, msg
+
+
+def test_backpropagation_hidden_biases():
+    for i in range(50):
+        if np.abs(q4.hidden_biases[i] - q3.hidden_biases[0][i]) > tol:
+            msg = f"hidden bias error at index: {i}"
+            assert False, msg
+
+
+def test_backpropagation_hidden_weights():
+    for i in range(64):
+        for j in range(50):
+            if np.abs(q4.hidden_weights[i, j] - q3.hidden_weights[0][i, j]) > tol:
+                msg = f"hidden weight error at row: {i} col: {j}"
                 assert False, msg
 
 
 if __name__ == "__main__":
     test_design_matrix_dimensions()
-    test_multilayer_with_one_layer()
+    test_initial_state_and_feedforward_output_weights()
+    test_initial_state_and_feedforward_output_biases()
+    test_initial_state_and_feedforward_hidden_weights()
+    test_initial_state_and_feedforward_hidden_biases()
+    test_initial_state_and_feedforward_hidden_neuron_input()
+    test_initial_state_and_feedforward_output_neuron_activation()
+    
+    test_backpropagation_probabilities()
+    test_backpropagation_output_error()
+    test_backpropagation_output_bias_gradient()
+    test_backpropagation_output_biases()
+    test_backpropagation_hidden_error()
+    test_backpropagation_output_weights()
+    test_backpropagation_hidden_biases()
+    test_backpropagation_hidden_weights
