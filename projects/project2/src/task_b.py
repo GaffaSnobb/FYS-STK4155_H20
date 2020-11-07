d5060a53ccd701bd1f2931b77e8ee36c9d1b2eea
jonkdahl <jon@dira.no>
2020-11-08 00:02:01 +0100

    Multilayer seems to be working. Add input handling in constructor.

--- projects/project2/src/task_b.py
+++ projects/project2/src/task_b.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
@@ -25,11 +26,20 @@ class FFNN(common._StatTools):
     """
     Class implementation of a feedforward neural network.
     """
-    def __init__(self, verbose=False):
+    def __init__(self, hidden_layer_sizes=(50,), verbose=False):
         """
         verbose : bool
             Toggle verbose mode on / off.
         """
+        try:
+            self.hidden_layer_sizes = hidden_layer_sizes
+            self.n_hidden_layers = len(self.hidden_layer_sizes)
+        except TypeError:
+            msg = f"hidden_layer_sizes must be of type {tuple}, {list},"
+            msg += f" or {np.ndarray}. Got {type(hidden_layer_sizes)}."
+            print(msg)
+            sys.exit()
+
         digits = datasets.load_digits()
         self.X = digits.images
         self.y = digits.target
@@ -37,14 +47,11 @@ class FFNN(common._StatTools):
         self.X = self.X.reshape(self.n_data_total, -1)
 
         self.n_features = self.X.shape[1]   # The number of features.
-        self.n_hidden_neurons = 50  # NOT IN USE WITH MULTI LAYER
+        
         self.n_epochs = 50
         self.batch_size = 20        # Size of each minibatch.
         self.n_categories = 10      # Number of output categories. 0, 1, 2, ...
 
-        # self.hidden_layer_sizes = (50, 50, 50)
-        self.hidden_layer_sizes = (50,)
-        self.n_hidden_layers = len(self.hidden_layer_sizes)
         self.verbose = verbose
 
 
@@ -74,30 +81,32 @@ class FFNN(common._StatTools):
 
         # self.neuron_activation[-1] = self.neuron_input[-2]@self.output_weights + self.output_biases
         self.neuron_activation[-1] = np.exp(self.neuron_input[-2]@self.output_weights + self.output_biases)
-        self.neuron_input[-1] = sigmoid(self.neuron_activation[-1])
+        self.neuron_input[-1] = sigmoid(self.neuron_activation[-1]) # CURRENTLY NOT IN USE
+        self.probabilities = self.neuron_activation[-1]/np.sum(self.neuron_activation[-1], axis=1, keepdims=True)
 
 
     def _backpropagation(self):
-        error = np.zeros(shape=self.n_hidden_layers + 1, dtype=np.ndarray)  # Store error for hidden layers and output layer (or is it input?).
-        error[-1] = cost(self.neuron_input[-1], self.y_minibatch)*dsigmoid(self.neuron_activation[-1])
-        error[-2] = self.output_weights@error[-1].T*dsigmoid(self.neuron_activation[-2]).T
+        self.error = np.zeros(shape=self.n_hidden_layers + 1, dtype=np.ndarray)  # Store error for hidden layers and output layer (or is it input?).
+        # self.error[-1] = cost(self.neuron_input[-1], self.y_minibatch)*dsigmoid(self.neuron_activation[-1])
+        self.error[-1] = cost(self.probabilities, self.y_minibatch)
+        self.error[-2] = self.output_weights@self.error[-1].T*dsigmoid(self.neuron_activation[-2]).T
 
         self.bias_gradient = np.zeros(shape=self.n_hidden_layers + 1, dtype=np.ndarray)
-        self.bias_gradient[-1] = np.sum(error[-1], axis=0)  # Why axis 0 here?
-        self.bias_gradient[-2] = np.sum(error[-2], axis=1)  # Why axis 1 here?
+        self.bias_gradient[-1] = np.sum(self.error[-1], axis=0)  # Why axis 0 here?
+        self.bias_gradient[-2] = np.sum(self.error[-2], axis=1)  # Why axis 1 here?
 
         self.weight_gradient = np.zeros(shape=self.n_hidden_layers + 1, dtype=np.ndarray)
-        self.weight_gradient[-1] = (error[-1].T@self.neuron_input[-2]).T # SHOULD THERE BE A TRANSPOSE HERE? Must be for dims. to match.
-        self.weight_gradient[-2] = (error[-2]@self.neuron_input[-3])
+        self.weight_gradient[-1] = (self.error[-1].T@self.neuron_input[-2]).T # SHOULD THERE BE A TRANSPOSE HERE? Must be for dims. to match.
+        self.weight_gradient[-2] = (self.error[-2]@self.neuron_input[-3])
 
         for i in range(-3, -self.n_hidden_layers - 2, -1):
             """
             Loop backwards through the errors, bias and weight
             gradients.
             """
-            error[i] = self.hidden_weights[i + 2]@error[i + 1]*dsigmoid(self.neuron_activation[i].T)
-            self.bias_gradient[i] = np.sum(error[i], axis=1)
-            self.weight_gradient[i] = error[i]@self.neuron_input[i - 1]
+            self.error[i] = self.hidden_weights[i + 2]@self.error[i + 1]*dsigmoid(self.neuron_activation[i].T)
+            self.bias_gradient[i] = np.sum(self.error[i], axis=1)
+            self.weight_gradient[i] = self.error[i]@self.neuron_input[i - 1]
 
         self.output_weights -= self.learning_rate*(self.weight_gradient[-1]) + self.lambd*self.output_weights
         self.output_biases -= self.learning_rate*(self.bias_gradient[-1])
@@ -183,8 +192,6 @@ class FFNN(common._StatTools):
 
                 self.feedforward()
                 self._backpropagation()
-            #     break
-            # break
 
         if self.verbose: self.stop_timing()
 
@@ -192,14 +199,7 @@ class FFNN(common._StatTools):
     def predict(self, X):
         self.X_minibatch = X
         self.feedforward()
-        # print(self.y_test)
-        # print(np.sum(self.neuron_input[-1][0]))
-        probabilities = self.neuron_activation[-1]/np.sum(self.neuron_activation[-1], axis=1, keepdims=True)
-        # print(np.sum(probabilities[-1]))
-        # print(np.argmax(probabilities, axis=1))
-        # print(self.y_test)
-        score = accuracy_score(np.argmax(probabilities, axis=1), self.y_test)
-        # print(score)
+        score = accuracy_score(np.argmax(self.probabilities, axis=1), self.y_test)
         return score
 
 
@@ -208,13 +208,13 @@ class FFNN(common._StatTools):
 
 
 if __name__ == "__main__":
-
-    # q2.train_neural_network_single(learning_rate=0.007)
-    # score = q2.predict_single(q2.X_test)
-    # print(score)
+    q1 = FFNN(hidden_layer_sizes=(50, 20, 20), verbose=True)
+    q1.train_neural_network(learning_rate=0.007)
+    score = q1.predict(q1.X_test)
+    print(score)
 
     # for learning_rate in np.logspace(-5, 0, 8):
-    #     q2.train_neural_network_single(learning_rate)
-    #     score = q2.predict_single(q2.X_test)
+    #     q1.train_neural_network_single(learning_rate)
+    #     score = q1.predict_single(q1.X_test)
     #     print(f"score: {score} for learning rate: {learning_rate}")
     pass
