6feabe5b0c40b2cbb672a7c30069777fca9371a8
jonkdahl <jon@dira.no>
2020-11-08 22:45:58 +0100

    Regression results seems to be popping out of the code now. The code is ugly as balls, with lots of debug info prints and temporary solutions. Beware!

--- projects/project2/src/task_b.py
+++ projects/project2/src/task_b.py
@@ -28,6 +28,9 @@ import common
 
 
 def neural_network_franke():
+    digits = datasets.load_digits()
+    # X = digits.images
+    # y = digits.target
     n_data_total = 200
     poly_degree = 3
 
@@ -36,13 +39,35 @@ def neural_network_franke():
     X = common.create_design_matrix_two_dependent_variables(x1,
         x2, n_data_total, poly_degree)
     y = common.franke_function(x1, x2)
+    n_features = X.shape[1]
+
+    # print(X.shape)
+    # print(y.shape)
+    # print(X.reshape(X.shape[0], -1).shape)
+    # print(digits.images.shape)
+    # print(digits.target.shape)
+    # print(digits.images.reshape(digits.images.shape[0], -1).shape)
     
-    q1 = common.FFNN(X=X, y=y, hidden_layer_sizes=(50, 20, 20),
-        hidden_layer_activation_function=common.sigmoid, verbose=True)
+    q1 = common.FFNN(X=X, y=y,
+        hidden_layer_sizes=(50, 20, 20), n_categories=1,
+        hidden_layer_activation_function=common.sigmoid,
+        output_activation_function=common.linear,
+        cost_function=common.cross_entropy_derivative,
+        verbose=False)
+    
+    # q1 = common.FFNN(X=digits.images, y=digits.target,
+    #     hidden_layer_sizes=(50, 20, 20), n_categories=10,
+    #     hidden_layer_activation_function=common.sigmoid,
+    #     output_activation_function=common.softmax,
+    #     cost_function=common.cross_entropy_derivative,
+    #     verbose=True)
     
     q1.train_neural_network(learning_rate=0.007)
-    score = q1.predict(q1.X_test)
-    print(score)
+    score = q1.predict_regression(q1.X_test)
+    for predicted, actual in zip(score, q1.y_test):
+        print(predicted, actual)
+    print(score.shape)
+    print(q1.y_test.shape)
 
 
 if __name__ == "__main__":
