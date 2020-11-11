dfab3181369966468d2920773e08500b33d97f62
jonkdahl <jon@dira.no>
2020-11-11 20:31:07 +0100

    Minor syntax.

--- projects/project2/src/task_b.py
+++ projects/project2/src/task_b.py
@@ -39,73 +39,54 @@ def classification():
     plt.show()
 
 
-def regression():
-    np.random.seed(1337)
+def regression_vary_learning_rate():
+    # np.random.seed(1337)
     n_data_total = 400
-    # x1 = np.random.uniform(0, 1, n_data_total)
-    x1 = np.linspace(0, 1, n_data_total)
-    # x2 = np.random.uniform(0, 1, n_data_total)
-    # X = np.zeros(shape=(n_data_total, 2))
-    # for i in range(n_data_total): X[i] = x1[i], x2[i]
-    # y = common.franke_function(x1, x2)
-
-    X = x1
-    y = x1 + x1**2 + np.random.normal(size=n_data_total)*0.1
+    x1 = np.random.uniform(0, 1, n_data_total)
+    x2 = np.random.uniform(0, 1, n_data_total)
+    X = np.zeros(shape=(n_data_total, 2))
+    for i in range(n_data_total): X[i] = x1[i], x2[i]
+    y = common.franke_function(x1, x2)
 
     q1 = common.FFNNRegressor(
         input_data = X,
         true_output = y,
-        hidden_layer_sizes=(50, 50),
+        hidden_layer_sizes=(50, 25, 25),
         n_categories = 1,
         n_epochs = 300,
-        batch_size = 30,
+        batch_size = 40,
         hidden_layer_activation_function = common.sigmoid,
         hidden_layer_activation_function_derivative = common.sigmoid_derivative,
         output_activation_function = common.linear,
         cost_function_derivative = common.mse_derivative,
         verbose = True,
         debug = False,
-        scaling = False)
-
-
-    # q1.train_neural_network(learning_rate=0.06)
-    # q1.X_selection = q1.X_test
-    # q1.feedforward()
-    # r_test = 1 - np.sum((q1.y_test - q1.neuron_input[-1])**2)/np.sum((q1.y_test - np.mean(q1.y_test))**2)
-    # print(f"{r_test=}")
-    # print("numerator test: ", np.sum((q1.y_test - q1.neuron_input[-1])**2))
-    # print("denominator test ", np.sum((q1.y_test - np.mean(q1.y_test))**2))
-    # print(f"{q1.y_test.shape=}")
-    # print(f"{q1.neuron_input[-1].shape=}")
-    # plt.plot(q1.X_test, q1.neuron_input[-1], "r.",label="test")
-    
-    # q1.X_selection = q1.X_train
-    # q1.feedforward()
-    # r_train = 1 - np.sum((q1.y_train - q1.neuron_input[-1])**2)/np.sum((q1.y_train - np.mean(q1.y_train))**2)
-    # print(f"{r_train=}")
-
-
-    # plt.plot(q1.X_train, q1.neuron_input[-1], "b.",label="train")
-    # plt.plot(X, y, label="actual")
-    # plt.legend()
-    # plt.show()
-
+        scaling = True)
 
     N = 10
-    learning_rates = np.logspace(-3, -1, N)
+    n_repetitions = 1   # Average to smooth the data.
+    learning_rates = np.linspace(1e-3, 2e-1, N)
     mse_train = np.zeros(N)
     mse_test = np.zeros(N)
     r_train = np.zeros(N)
     r_test = np.zeros(N)
 
-    for i in range(N):
-        q1.train_neural_network(learning_rate=learning_rates[i])
-        q1.score()
-        mse_train[i] = q1.mse_train
-        mse_test[i] = q1.mse_test
-        r_train[i] = q1.r_train
-        r_test[i] = q1.r_test
+    for rep in range(n_repetitions):
+        print(f"\nrepetition {rep+1} of {n_repetitions}")
+        
+        for i in range(N):
+            print(f"{i+1} of {N}, {learning_rates[i]=}")
+            q1.train_neural_network(learning_rate=learning_rates[i])
+            q1.score()
+            mse_train[i] += q1.mse_train
+            mse_test[i] += q1.mse_test
+            r_train[i] += q1.r_train
+            r_test[i] += q1.r_test
 
+    mse_train /= n_repetitions
+    mse_test /= n_repetitions
+    r_train /= n_repetitions
+    r_test /= n_repetitions
 
     plt.plot(learning_rates, mse_train, label="train")
     plt.plot(learning_rates, mse_test, label="test")
@@ -120,7 +101,6 @@ def regression():
     plt.ylabel("r_score")
     plt.legend()
     plt.show()
-
 
 
 def logistic():
@@ -139,7 +119,8 @@ def logistic():
         debug = False,
         scaling = False)
 
+
 if __name__ == "__main__":
-    classification()
-    # regression()
+    # classification()
+    regression_vary_learning_rate()
     pass
