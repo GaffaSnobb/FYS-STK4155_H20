8875dc68e3bf34a13fbcff3d1b673f44bcb07048
jonkdahl <jon@dira.no>
2020-12-14 19:51:27 +0100

    Framework for future prediction.

--- projects/project3/src/analyse_dropout_price_comparison.py
+++ projects/project3/src/analyse_dropout_price_comparison.py
@@ -1,6 +1,7 @@
+import sys
 import matplotlib.pyplot as plt
 import numpy as np
-from lstm import CryptoPrediction
+from lstm import CryptoPrediction, to_sequences
 
 
 def plot_price():
@@ -8,7 +9,9 @@ def plot_price():
     n_dropout_rates = len(dropout_rates)
     n_repetitions = 1
 
-    Y_predicted = np.empty((59, n_dropout_rates))
+    days = 59
+    end_day = days - 30
+    Y_predicted = np.empty((days, n_dropout_rates))
 
     for i in range(n_dropout_rates):
 
@@ -25,21 +28,23 @@ def plot_price():
         )
         q.train_model(n_repetitions)
 
-        Y_predicted[:, i] = q.scaler.inverse_transform(q.model.predict(q.X_test)).ravel()
+        Y_predicted[:end_day, i] = q.scaler.inverse_transform(q.model.predict(q.X_test[:end_day])).ravel()
+        print(f"{q.X_test[:end_day].shape=}")
+        print(f"{Y_predicted[:end_day, i].shape=}")
+        print(f"{to_sequences(data = Y_predicted[:end_day, i].reshape(-1, 1), seq_len = 1).shape=}")
+        
+
+        sys.exit()
+        for j in range(days - end_day):
+            next_data = to_sequences(data = Y_predicted[:end_day + j, i].reshape(-1, 1), seq_len = 1)
+            Y_predicted[:end_day + 1 + j, i] = q.model.predict(next_data).ravel()
 
     y_test = q.scaler.inverse_transform(q.y_test)
-    # y_train = q.scaler.inverse_transform(q.y_train)
-    # y_predicted = q.scaler.inverse_transform(y_predicted)
 
-    # # plt.plot(q.price, label="price")
-    # plt.plot(y_test, label="y_test")
-    # # plt.plot(y_train, label="y_train")
-    # # plt.plot(np.concatenate((y_train, y_test)), label="conc")
 
     fig, ax = plt.subplots(figsize = (9, 7))
-
-    ax.plot(Y_predicted[:, 0], label = f"Dropout rate: {dropout_rates[0]}", color = "black", linestyle = "dashed")
-    ax.plot(Y_predicted[:, 2], label = f"Dropout rate: {dropout_rates[2]}", color = "black", linestyle = "dotted")
+    # ax.plot(Y_predicted[:, 0], label = f"Dropout rate: {dropout_rates[0]}", color = "black", linestyle = "dashed")
+    # ax.plot(Y_predicted[:, 2], label = f"Dropout rate: {dropout_rates[2]}", color = "black", linestyle = "dotted")
     ax.plot(Y_predicted[:, 4], label = f"Dropout rate: {dropout_rates[4]}", color = "black", linestyle = "solid")
     ax.plot(y_test, label = f"Actual", color = "grey")
     ax.set_title('BTC prediction', fontsize = 15)
@@ -52,6 +57,44 @@ def plot_price():
     plt.show()
 
 
+def plot_price_v2():
+
+    dropout_rates = [0, 0.2, 0.4, 0.6, 0.8]
+    n_dropout_rates = len(dropout_rates)
+    how_many = 100
+    n_predictions = 10
+    scope = np.arange(how_many)
+
+    for i in range(n_dropout_rates):
+
+        q = CryptoPrediction(
+            seq_len = 100,
+            train_size = 0.95,
+            dropout = dropout_rates[i],
+            batch_size = 64,
+            epochs = 90,
+            data_start = 1500,
+            neurons = 50,
+            csv_path = "data/btc-usd-max.csv",
+            directory = "analyse_dropout/"
+        )
+        q.train_model()
+
+        
+        scaled_price = list(q.scaled_price[-how_many - n_predictions:-n_predictions])
+        for _ in range(n_predictions):
+            sequence = to_sequences(scaled_price, how_many - 1)
+            prediction = q.model.predict(sequence).ravel()
+            scaled_price.append(prediction)
+            scaled_price.pop(0)
+
+        plt.plot(scope[-n_predictions - 1:], scaled_price[-n_predictions - 1:], label = f"{dropout_rates[i]=}", linestyle = "dashed")
+    
+    plt.plot(scope, q.scaled_price[-how_many:], label = "true")
+    plt.legend()
+    plt.show()
+
+
 if __name__ == "__main__":
-    plot_price()
+    plot_price_v2()
     pass
